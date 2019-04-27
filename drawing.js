8ea59744a1630b42be086385b7720786a236604e
evblance <evblance@users.noreply.github.com>
2019-04-27 11:51:40 +1000

    Changes canvas draw radius periodically with time

--- drawing.js
+++ drawing.js
@@ -12,11 +12,26 @@ const PEN_STATE = Object.freeze({
 // Larger values will change the fill colour at a slower rate
 const COLOUR_RATE = 1000;
 
+// Larger values will change the brush size at a slower rate
+const SIZE_RATE = 500;
+
 // We track the pen's drawing state so we know when to let the user draw
 let penState = PEN_STATE.INACTIVE;
 
 // Keep a global timestamp to track of draw start time
 let drawStartTime;
+
+/**
+ * Returns a number radius for the drawing brush size based on a periodic cycle of time.
+ */
+const getBrushSizeFromTime = () => {
+  if (!drawStartTime) {
+    return 10;
+  }
+  const t = Date.now() - drawStartTime;
+  const radius = 10 + 5 * Math.sin(t / SIZE_RATE);
+  return radius;
+};
 
 /**
  * Returns an HTML colour string based on a periodic cycle of time.
@@ -36,7 +51,7 @@ const getFillStyleFromTime = () => {
  * @param {number} y The y coordinate on the canvas.
  */
 const draw = (x, y) => {
-  const radius = 10;
+  const radius = getBrushSizeFromTime();
   const startAngle = 0;
   const endAngle = 2 * Math.PI;
   context.fillStyle = getFillStyleFromTime();
